d29c197cdf23afc9ffabe89b103026031f8738ee
Ashir Imran <71806327+xlinkfrostx@users.noreply.github.com>
2022-07-10 23:26:19 -0400

    feat: added Skills and Interests, Footer

---
+++ components/SkillsInterests.js
@@ -0,0 +1,32 @@
+export default function SkillsInterests() {
+  return (
+    <div className="flex flex-col justify-between content-center max-w-3xl mb-5 w-full border-b-2 border-red-200 border-opacity-100">
+      <h2 className="font-bold text-xl sm:text-3xl text-white mb-5">Skills and Interests</h2>
+      <div className="flex justify-center">
+        <h2 className="text-md sm:text-xl text-white mb-3">Programming Languages & SWE Tools</h2>
+      </div>
+      <div className="grid grid-cols-3 sm:grid-cols-9 gap-5 justify-between mb-5">
+        <img style={{ height: "50px", width: "auto" }} src="/img/JSLogo.png" alt="Javascript"></img>
+        <img style={{ height: "50px", width: "auto" }} src="/img/HTMLLogo.png" alt="HTML"></img>
+        <img style={{ height: "50px", width: "auto" }} src="/img/CSSLogo.png" alt="CSS"></img>
+        <img style={{ height: "50px", width: "auto" }} src="/img/ReactLogo.png" alt="React"></img>
+        <img style={{ height: "50px", width: "auto" }} src="/img/PythonLogo.png" alt="Python"></img>
+        <img style={{ height: "50px", width: "auto" }} src="/img/CLogo.png" alt="C"></img>
+        <img style={{ height: "50px", width: "auto" }} src="/img/JavaLogo.png" alt="Java"></img>
+        <img style={{ height: "50px", width: "auto" }} src="/img/MongoDBLogo.png" alt="MongoDB"></img>
+        <img style={{ height: "50px", width: "auto" }} src="/img/PostgreSQLLogo.png" alt="PostgreSQL"></img>
+      </div>
+      <div className="flex justify-center">
+        <h2 className="text-md sm:text-xl text-white mb-2">Areas of Interest</h2>
+      </div>
+      <div className="inline-grid grid-cols-3 sm:grid-cols-3 gap-4 sm:gap-5 text-gray-400 mb-5">
+        <p className="flex justify-center align-center">Full Stack Web Development</p>
+        <p className="flex justify-center align-center">Artificial Intelligence</p>
+        <p className="flex justify-center align-center">Machine Learning</p>
+        <p className="flex justify-center align-center">Mobile App Development</p>
+        <p className="flex justify-center align-center">Reverse Engineering</p>
+        <p className="flex justify-center align-center">UI/UX Design</p>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,19 @@ import Header from "../components/Header.js";
 import Navbar from "../components/Navbar.js";
 import DividerLine from "../components/DividerLine.js";
 import Projects from "../components/Projects.js";
+import SkillsInterests from "../components/SkillsInterests.js";
+import Footer from "../components/Footer.js";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center px-8">
+    <div className="flex flex-col justify-center items-center px-8 pb-16">
       <Navbar></Navbar>
       <main className="overflow-y-scroll flex flex-col justify-center max-w-3xl w-full">
         <Header></Header>
+        <SkillsInterests></SkillsInterests>
         <Projects></Projects>
       </main>
+      <Footer></Footer>
     </div>
   );
 }
