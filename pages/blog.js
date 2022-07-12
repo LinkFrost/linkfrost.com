e932f88bb300b8fe25f163afbecdaa68f59d6b46
Ashir Imran <71806327+xlinkfrostx@users.noreply.github.com>
2022-07-12 11:35:21 -0400

    fix: html titles

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Navbar from "../components/Navbar.js";
 import Footer from "../components/Footer.js";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center px-8 pb-16">
+      <Head>
+        <title>Blog - LinkFrost</title>
+      </Head>
       <Navbar></Navbar>
       <main className="flex flex-col justify-center items-center max-w-3xl w-full">
         <div className="">
