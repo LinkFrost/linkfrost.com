4c29b1b9c22a9ae510b14a920d57a6aae6505651
LinkFrost <xlinkfrostx@gmail.com>
2022-08-06 12:38:56 -0400

    fix: UI adjustments, color scheme

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="flex flex-col sm:flex-row justify-between content-center max-w-3xl mt-5 mb-5 w-full border-b-2 border-red-200 border-opacity-100">
+    <div className="flex flex-col sm:flex-row justify-between content-center max-w-3xl mt-5 mb-5 w-full border-b-0 border-red-200 border-opacity-100">
       <div className="flex flex-col pr-8">
         <h1 className="font-bold text-3xl sm:text-5xl text-white">Ashir Imran</h1>
         <h2 className="text-xl sm:text-3xl text-red-300 mb-5">aka LinkFrost</h2>
