7a0ef1f6520547b73b3bd441a8408b0614b5f1f7
Ashir Imran <71806327+xlinkfrostx@users.noreply.github.com>
2022-11-26 13:42:11 -0500

    fix: css mobile fixes - safari

--- components/SkillsInterests.js
+++ components/SkillsInterests.js
@@ -6,15 +6,15 @@ export default function SkillsInterests() {
         <h2 className="text-md sm:text-xl text-white mb-3">Programming Languages & SWE Tools</h2>
       </div>
       <div className="grid grid-cols-3 sm:grid-rows-2 sm:grid-cols-5 md:grid-rows-none md:grid-cols-9 md:gap-10 gap-12 justify-between m-auto mb-5">
-        <img style={{ height: "50px", width: "auto" }} src="/img/JSLogo.png" alt="Javascript"></img>
-        <img style={{ height: "50px", width: "auto" }} src="/img/HTMLLogo.png" alt="HTML"></img>
-        <img style={{ height: "50px", width: "auto" }} src="/img/CSSLogo.png" alt="CSS"></img>
-        <img style={{ height: "50px", width: "auto" }} src="/img/ReactLogo.png" alt="React"></img>
-        <img style={{ height: "50px", width: "auto" }} src="/img/PythonLogo.png" alt="Python"></img>
-        <img style={{ height: "50px", width: "auto" }} src="/img/CLogo.png" alt="C"></img>
-        <img style={{ height: "50px", width: "auto" }} src="/img/JavaLogo.png" alt="Java"></img>
-        <img style={{ height: "50px", width: "auto" }} src="/img/MongoDBLogo.png" alt="MongoDB"></img>
-        <img style={{ height: "50px", width: "auto" }} src="/img/PostgreSQLLogo.png" alt="PostgreSQL"></img>
+        <img style={{ position: "relative", height: "50px", width: "auto" }} src="/img/JSLogo.png" alt="Javascript"></img>
+        <img style={{ position: "relative", height: "50px", width: "auto" }} src="/img/HTMLLogo.png" alt="HTML"></img>
+        <img style={{ position: "relative", height: "50px", width: "auto" }} src="/img/CSSLogo.png" alt="CSS"></img>
+        <img style={{ position: "relative", height: "50px", width: "auto" }} src="/img/ReactLogo.png" alt="React"></img>
+        <img style={{ position: "relative", height: "50px", width: "auto" }} src="/img/PythonLogo.png" alt="Python"></img>
+        <img style={{ position: "relative", height: "50px", width: "auto" }} src="/img/CLogo.png" alt="C"></img>
+        <img style={{ position: "relative", height: "50px", width: "auto" }} src="/img/JavaLogo.png" alt="Java"></img>
+        <img style={{ position: "relative", height: "50px", width: "auto" }} src="/img/MongoDBLogo.png" alt="MongoDB"></img>
+        <img style={{ position: "relative", height: "50px", width: "auto" }} src="/img/PostgreSQLLogo.png" alt="PostgreSQL"></img>
       </div>
       <div className="flex justify-center">
         <h2 className="text-md sm:text-xl text-white mb-2">Areas of Interest</h2>
