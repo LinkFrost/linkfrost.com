61dec12b45a3b252f5b06d07728c563408b0918c
Ashir Imran <71806327+xlinkfrostx@users.noreply.github.com>
2022-11-26 13:21:04 -0500

    meta data 4

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -64,7 +64,8 @@ export default function BlogPostPage({ slug, frontMatter, content }) {
         <meta name="twitter:site" content="LinkFrost.com"></meta>
         <meta name="twitter:title" content={title}></meta>
         <meta name="twitter:description" content={excerpt}></meta>
-        <meta name="twitter:card" content="img/Cropped.png"></meta>
+        <meta name="twitter:image" content="img/Cropped.png"></meta>
+        <meta name="twitter:card" content="summary_small_image"></meta>
         <title>{title} - LinkFrost</title>
       </Head>
       <Navbar></Navbar>
